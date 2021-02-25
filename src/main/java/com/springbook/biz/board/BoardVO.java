9c574a34eb76505be131bbb90ad2fa437bdfbaaf
tseop <77163842+tseop@users.noreply.github.com>
2021-02-26 03:30:45 +0900

    fileUpload Commit 4days Class05

--- src/main/java/com/springbook/biz/board/BoardVO.java
+++ src/main/java/com/springbook/biz/board/BoardVO.java
@@ -2,6 +2,8 @@ package com.springbook.biz.board;
 
 import java.sql.Date;
 
+import org.springframework.web.multipart.MultipartFile;
+
 //VO(Value Object)
 public class BoardVO {
 	private int seq;
@@ -10,6 +12,33 @@ public class BoardVO {
 	private String content;
 	private Date regDate;
 	private int cnt;
+	private String searchCondition;
+	private String searchKeyword;
+	private MultipartFile uploadFile;
+
+	public MultipartFile getUploadFile() {
+		return uploadFile;
+	}
+
+	public void setUploadFile(MultipartFile uploadFile) {
+		this.uploadFile = uploadFile;
+	}
+
+	public String getSearchCondition() {
+		return searchCondition;
+	}
+
+	public void setSearchCondition(String searchCondition) {
+		this.searchCondition = searchCondition;
+	}
+
+	public String getSearchKeyword() {
+		return searchKeyword;
+	}
+
+	public void setSearchKeyword(String searchKeyword) {
+		this.searchKeyword = searchKeyword;
+	}
 
 	public int getSeq() {
 		return seq;
--- src/main/webapp/insertBoard.jsp
+++ src/main/webapp/insertBoard.jsp
@@ -25,6 +25,10 @@
 					<td align="left"><textarea name="content" cols="40" rows="10"></textarea></td>
 				</tr>
 				<tr>
+					<td bgcolor = "orange" width="70">업로드</td><td align="left">
+					<input type="file" name="uploadFile"/></td>
+				</tr>
+				<tr>
 					<td colspan="2" align="center"><input type="submit"
 						value=" 새글 등록 " /></td>
 				</tr>
